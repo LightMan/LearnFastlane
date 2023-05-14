68b12d92bd7210ff347200df4118eb3e7ae4a7fe
David Jorge <david.jorge@appconcept.es>
2023-05-14 15:43:44 +0200

    Initial Commit

---
+++ Shared/LearnFastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnFastlaneApp.swift
+//  Shared
+//
+//  Created by djorge on 14/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnFastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
